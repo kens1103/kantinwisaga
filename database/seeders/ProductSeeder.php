a3a3f739820441bc4e550a3d49a8bc0e6f5dc1d5
kens1103 <kenescahyaning@gmail.com>
2024-05-15 15:00:50 +0700

    mengubah nama makanan

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -11,7 +11,7 @@ class ProductSeeder extends Seeder
     {
         DB::table('products')->insert([
             ['name' => 'onigiri', 'description' => 'enak loh...', 'price' => 3000, 'stock' => 10, 'photo' => 'products/onigiri.jpeg', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'jus', 'description' => 'enak nih ...', 'price' => 5000, 'stock' => 31, 'photo' => 'products/jus.jpeg', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'minuman', 'description' => 'enak nih ...', 'price' => 5000, 'stock' => 31, 'photo' => 'products/jus.jpeg', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'martabak manis', 'description' => 'enak banget...', 'price' => 3000, 'stock' => 45, 'photo' => 'products/martabak_manis.jpeg', 'created_at' => now(), 'updated_at' => now()],
             // Tambahkan data produk lainnya sesuai kebutuhan
         ]);
